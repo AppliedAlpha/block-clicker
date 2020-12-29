bd91304ec6f60c74cb89909d5118fe9bdec0e06f
Aria <appliedalpha@naver.com>
2020-12-30 01:42:19 +0900

    0.3 FireBase Linking

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import {firebaseInstance} from "fBase";
 
 const App = () => {
+    const firebaseApp = firebaseInstance.app;
+
     return (
         <div className="App">
             <h1>Hi</h1>
